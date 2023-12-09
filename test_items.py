91de6bcf82b6d01d930c10e67fe6a4a3feb51a42
Алексей Александров <93279148+enc1cxx@users.noreply.github.com>
2023-12-09 20:23:56 +0300

    Delete .idea directory

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 def test_button_present(browser):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(3)
+    time.sleep(30)
     assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket").is_displayed(), ("the button must be "
                                                                                               "present on the page")
 
