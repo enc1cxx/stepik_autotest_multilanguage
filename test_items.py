aa3e3138c33a598bd3badbebd0b6c308779a327f
Aleksey Aleksandrov <yohoho4721@gmail.com>
2023-12-09 20:16:08 +0300

    this work is done

---
+++ test_items.py
@@ -0,0 +1,13 @@
+import time
+
+from selenium.webdriver.common.by import By
+
+
+def test_button_present(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(3)
+    assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket").is_displayed(), ("the button must be "
+                                                                                              "present on the page")
+
+
